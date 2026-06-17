91e417bf1eb94b1e4ede0b7f7b69dbfbb4ac71ec
Riya1338 <91661138+Riya1338@users.noreply.github.com>
2026-06-17 15:59:21 +1200

    Update Advisor.java

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,11 +1,11 @@
 package com.wellsfargo.counselor.entity;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
+import java.util.List;
 @Entity
 public class Advisor {
 
